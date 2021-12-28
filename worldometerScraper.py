3f834cdb79b81b52dc68b099c1c5d197dc34c079
Kevin Joseph Scaria <52093773+kevinscaria@users.noreply.github.com>
2021-12-28 17:23:38 +0530

    Update worldometerScraper.py

--- worldometerScraper.py
+++ worldometerScraper.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from datetime import datetime, timedelta
 from gazpacho import Soup
 
@@ -42,6 +43,8 @@ df.to_csv('data/worldometerTimeSeriesData.csv', index = False)
 
 #Download district wise vax data
 vaxDf = pd.read_csv('https://data.incovid19.org/csv/latest/cowin_vaccine_data_districtwise.csv', dtype=object)
+new_col_names = np.array(vaxDf.columns) + '._' + np.array(vaxDf.iloc[0, :].values)
+vaxDf.columns = new_col_names
 vaxDf = vaxDf[vaxDf['State'].notnull()]
 vaxDf.to_csv("data/vaxDf.csv", index=False)
 
@@ -66,65 +69,3 @@ indDf.to_csv("data/indDf.csv", index=False)
 indDfS = pd.read_csv('https://data.incovid19.org/csv/latest/states.csv')
 indDfS = indDfS[(indDfS['State'] != 'India') & (indDfS['State'] != 'State Unassigned')]
 indDfS.to_csv("data/indDfS.csv", index=False)
-
-
-##########################################################################################################
-
-
-#Data from India MOHFW - Statewise Cases
-soup = Soup.get('https://www.mygov.in/corona-data/covid19-statewise-status')
-
-val_list = {}
-for idx, field_item in enumerate(soup.find("div", {"class": "field-items"}, mode="all")):
-    val_list[idx] = field_item.find("div", {"class": "field-item even"}, mode="all")
-
-state_dict = {}
-
-#Indirect Cases
-for tgs in val_list[15]:
-    for child in tgs.find("div", {"class": "field-item even"}, mode="all"):
-        try:
-            subdiv = child.find("div", {"class": "field-collection-view clearfix view-mode-full"}, mode="first")
-            subdiv = subdiv.find("div", {"class": "entity entity-field-collection-item field-collection-item-field-covid-statewise-data clearfix"}, mode="first")
-            subdiv = subdiv.find("div", {"class": "content"}, mode="first")
-            state = subdiv.find("div", {"class": "field field-name-field-select-state field-type-list-text field-label-above"}, mode="first")
-            state = state.find("div", {"class": "field-items"}, mode="first")
-            state = state.find("div", {"class": "field-item even"}, mode="first").text
-
-            state_dict[state] = []
-            state_dict[state].append(today)
-
-            confirmed = subdiv.find("div", {"class": "field field-name-field-total-confirmed-indians field-type-number-integer field-label-above"}, mode="first")
-            confirmed = confirmed.find("div", {"class": "field-items"}, mode="first")
-            confirmed = int(confirmed.find("div", {"class": "field-item even"}, mode="first").text)
-            state_dict[state].append(confirmed)
-
-            cured = subdiv.find("div", {"class": "field field-name-field-cured field-type-number-integer field-label-above"}, mode="first")
-            cured = cured.find("div", {"class": "field-items"}, mode="first")
-            cured = int(cured.find("div", {"class": "field-item even"}, mode="first").text)
-            state_dict[state].append(cured)
-
-            deaths = subdiv.find("div", {"class": "field field-name-field-deaths field-type-number-integer field-label-above"}, mode="first")
-            deaths = deaths.find("div", {"class": "field-items"}, mode="first")
-            deaths = int(deaths.find("div", {"class": "field-item even"}, mode="first").text)
-            state_dict[state].append(deaths)
-        except:
-            pass
-
-#Direct Cases
-direct_cases = soup.find("div", {"class": "field-item even"}, mode="all")
-for idx, i in enumerate(direct_cases):
-    if i.text == 'State Name:':
-        state_dict[direct_cases[idx+1].text] = [today] + [int(j.text) for j in direct_cases[idx+2:idx+5]]
-
-indDfs_mohfw = pd.DataFrame(state_dict, index = ['date', 'Confirmed', 'Recovered', 'Death']).T.reset_index()
-indDfs_mohfw = indDfs_mohfw.rename(columns = {'index':'State'})
-indDfs_mohfw.to_csv("data/indDfS_MOHFW.csv", index=False)
-
-#Load data dump
-mohfwdf = pd.read_csv('data/indDfS_MOHFW.csv')
-
-#Append new data
-mohfwdf = mohfwdf.append(indDfs_mohfw)
-mohfwdf = mohfwdf.drop_duplicates(subset = ['date', 'State'])
-mohfwdf.to_csv('data/indDfS_MOHFW.csv', index = False)
